1355ad78b767950964b58fe234b3e121ee3b1b66
Ukhang <ukhangmarma459@gmail.com>
2025-01-30 18:16:20 +0600

    sidebar item

--- src/app/(main)/_components/Navigation.tsx
+++ src/app/(main)/_components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -134,6 +134,11 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
+            onClick={() => {}}
+          />
+          <Item
+            label="Settings"
+            icon={Settings}
             onClick={() => {}}
           />
           <Item label={"New page"} onClick={handleCreate} icon={PlusCircle} />
