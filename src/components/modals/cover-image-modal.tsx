bda3dc7589285d75a61dbde048283c51430d7c5c
Ukhang <ukhangmarma459@gmail.com>
2025-02-03 14:38:25 +0600

    advanced file upload

--- src/components/modals/cover-image-modal.tsx
+++ src/components/modals/cover-image-modal.tsx
@@ -11,50 +11,53 @@ import { useParams } from "next/navigation";
 import { Id } from "../../../convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
-    const [file, setFile] = useState<File>();
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const { edgestore } = useEdgeStore();
-    const coverImage = useCoverImage();
-    const update = useMutation(api.documents.update);
-    const params = useParams();
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { edgestore } = useEdgeStore();
+  const coverImage = useCoverImage();
+  const update = useMutation(api.documents.update);
+  const params = useParams();
 
-    const onClose = () => {
-        setFile(undefined);
-        setIsSubmitting(false);
-        coverImage.onClose();
-    };
-    
-    const onChange = async (file?: File) => {
-        if (file) {
-            setIsSubmitting(true);
-            setFile(file);
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
 
-            const res = await edgestore.publicFiles.upload({
-                file
-            });
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
 
-            await update({
-                id: params.documentId as Id<"documents">,
-                coverImage: res.url
-            });
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
 
-            onClose();
-        }
-    };
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
 
-    return (
-        <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <h2 className="text-center text-lg font-semibold">Cover Image</h2>
-                </DialogHeader>
-                <SingleImageDropzone
-                    className="w-full outline-none"
-                    disabled={isSubmitting}
-                    value={file}
-                    onChange={onChange}
-                />
-            </DialogContent>
-        </Dialog>
-    )
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
+      </DialogContent>
+    </Dialog>
+  );
 };
