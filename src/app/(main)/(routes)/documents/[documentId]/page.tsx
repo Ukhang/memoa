fb34319885343c5a506b160ff474cfe8860f5934
Ukhang <ukhangmarma459@gmail.com>
2025-02-03 13:42:00 +0600

    add delete cover image

--- src/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ src/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -5,6 +5,7 @@ import { Id } from "../../../../../../convex/_generated/dataModel";
 import { api } from "../../../../../../convex/_generated/api";
 import { Toolbar } from "@/components/toolbar";
 import React from "react"; // Import React
+import { Cover } from "@/components/cover";
 
 interface DocumentIdPageProps {
     params: {
@@ -27,7 +28,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
     return (
         <div className="pb-40">
-            <div className="h-[35vh]" />
+            <Cover url={document.coverImage}/>
             <div className="md:max-w-3xl lg:md-max-4xl mx-auto">
                 <Toolbar initialData={document} />
             </div>
